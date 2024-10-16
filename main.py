62c5140d7af095a28ffceeae281a14291272d60e
Malcolm Walter <malcolm.walter@etud.u-picardie.fr>
2024-10-16 10:26:54 +0200

    Création du Main avec Pygame

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World")
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 1080,720
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Jeu de Dames")
+
+def main():
+
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
+    pygame.quit()
+ 
+
+if __name__ == "__main__":
+    main()
