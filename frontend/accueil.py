e35984d0a6b7227e512caa3d2c008baafa1f58e6
Malcolm Walter <malcolm.walter@etud.u-picardie.fr>
2024-11-19 18:04:24 +0100

    Ajout dynamique du chemin racine du projet

--- frontend/accueil.py
+++ frontend/accueil.py
@@ -1,5 +1,11 @@
 ﻿import tkinter as tk
 import customtkinter as ctk
+import sys
+import os
+
+# Ajouter dynamiquement le chemin racine du projet
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 from Projects2024.backend.attributs_de_jeu.Board import Board
 
 # Configuration de base pour customtkinter
